cd94d605cf6d32940d6bdef02122971bf8dcfe83
ARTSIOM KUKHAREV <xeontem@gmail.com>
2016-08-19 14:22:37 +0300

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -462,7 +462,7 @@ var resizePizzas = function(size) {
   window.performance.mark("mark_end_resize");
   window.performance.measure("measure_pizza_resize", "mark_start_resize", "mark_end_resize");
   var timeToResize = window.performance.getEntriesByName("measure_pizza_resize");
-  console.log("Time to resize pizzas: " + timeToResize[timeToResize.length-1].duration + "ms");
+  console.log(`Time to resize pizzas: ${timeToResize[timeToResize.length-1].duration} ms);
 };
 
 window.performance.mark("mark_start_generating"); // collect timing data
@@ -477,7 +477,7 @@ for (var i = 2; i < 100; i++) {
 window.performance.mark("mark_end_generating");
 window.performance.measure("measure_pizza_generation", "mark_start_generating", "mark_end_generating");
 var timeToGenerate = window.performance.getEntriesByName("measure_pizza_generation");
-console.log("Time to generate pizzas on load: " + timeToGenerate[0].duration + "ms");
+console.log(`Time to generate pizzas on load: ${timeToGenerate[0].duration} ms`);
 
 // Iterator for number of times the pizzas in the background have scrolled.
 // Used by updatePositions() to decide when to log the average time per frame
@@ -490,7 +490,7 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
   for (var i = numberOfEntries - 1; i > numberOfEntries - 11; i--) {
     sum = sum + times[i].duration;
   }
-  console.log("Average scripting time to generate last 10 frames: " + sum / 10 + "ms");
+  console.log(`Average scripting time to generate last 10 frames: ${sum / 10} ms`);
 }
 
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
